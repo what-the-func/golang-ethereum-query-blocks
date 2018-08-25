a61742f8eccd85488266cf196fc6683d2a3869ca
what-the-func <ezynda@what-the-func.com>
2018-08-25 14:23:56 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://mainnet.infura.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get the latest block
+	header, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber := header.Number
+	fmt.Println("Latest block: " + blockNumber.String())
+
+	// Query a specific block by number
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Block time: " + block.Time().String())
+	fmt.Println("Block difficulty: " + block.Difficulty().String())
+	fmt.Println("Block hash: " + block.Hash().String())
+	fmt.Println("Number of TXs: " + strconv.Itoa(len(block.Transactions())))
+
+	// Query a specific block by number
+	blockByHash, err := client.BlockByHash(context.Background(), block.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Block time: " + blockByHash.Time().String())
+	fmt.Println("Block difficulty: " + blockByHash.Difficulty().String())
+	fmt.Println("Block hash: " + blockByHash.Hash().String())
+	fmt.Println("Number of TXs: " + strconv.Itoa(len(blockByHash.Transactions())))
+}
